30d4f407a19fc8dd93d794754030c7851cd79405
AliAS-86 <asalman.htx@gmail.com>
2025-01-16 23:22:23 -0600

    adding first practice code - Hello.java

---
+++ practice/src/main/java/practices/Hello.java
@@ -0,0 +1,7 @@
+// Hello class declaration
+public class Hello {
+    // Main method declaration
+    public static void main(String[] args) {
+        System.out.println("Hello to AliAS Project");
+    }
+}
