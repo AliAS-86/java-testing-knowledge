1ae8a12a204b4e23129345a405c881b7bfb0a578
AliAS-86 <asalman.htx@gmail.com>
2025-01-17 09:47:50 -0600

    commit after first mvn compile

--- src/main/java/com/alias/qatestingfw/Hello.java
+++ src/main/java/com/alias/qatestingfw/Hello.java
@@ -1,4 +1,5 @@
 // Declaring class Hello
+package com.alias.qatestingfw;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Welcome to the AliAS Project, Practice Session");
